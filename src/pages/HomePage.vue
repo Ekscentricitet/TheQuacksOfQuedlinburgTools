5cd0a764e46eda60374bdffc8c1691b14d8ce1e8
StefanStoimenov <49730291+Ekscentricitet@users.noreply.github.com>
2024-05-06 19:23:23 +0300

    Group chips.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,16 @@
 <template>
-  <ChipComponent v-for="(chip, index) in chips" :key="index" v-model="chips[index]" />
+  <template v-for="(group, index) in groupedItems" :key="index">
+    <div class="row">
+      <ChipComponent v-for="chip in group.items" :key="chip.name" v-model="chips[getElementIndex(chip)]" />
+    </div>
+  </template>
   <DrawComponent v-model="chips"></DrawComponent>
 </template>
 
 <script setup lang="ts">
 import ChipComponent from 'components/ChipComponent.vue'
 import DrawComponent from 'components/DrawComponent.vue'
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import chipsData from 'assets/chips.json';
 import Chip from 'components/models/chip';
 
@@ -15,6 +19,25 @@ const chips = ref<Chip[]>([]);
 onMounted(() => {
   chips.value = Chip.fromDtos(chipsData);
 });
+
+const groupedItems = computed(() => {
+  const groups: Record<string, Chip[]> = {};
+  chips.value.forEach(chip => {
+    const name = chip.name;
+    if (!groups[name]) {
+      groups[name] = [];
+    }
+    groups[name].push(chip);
+  });
+  return Object.keys(groups).map(name => ({
+    name,
+    items: groups[name],
+  }));
+});
+
+const getElementIndex = (chip: Chip) => {
+  return chips.value.findIndex(item => item.name === chip.name && item.value == chip.value);
+};
 </script>
 
 <style></style>
