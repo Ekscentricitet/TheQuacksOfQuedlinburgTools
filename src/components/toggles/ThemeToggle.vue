e373e7f33726294dc6052a0a57cc9b9a316d1c84
StefanStoimenov <49730291+Ekscentricitet@users.noreply.github.com>
2024-05-10 17:24:25 +0300

    Fix capitalization.

---
+++ src/components/toggles/ThemeToggle.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-toggle
+    v-model="currentTheme"
+    label="Dark Mode"
+    color="primary"
+    @update:model-value="toggleTheme"
+  />
+</template>
+
+<script setup>
+import { useSettingsStore } from "stores/settingsStore";
+import { useQuasar } from "quasar";
+import { onMounted, ref } from "vue";
+
+const settingsStore = useSettingsStore();
+const $q = useQuasar();
+const currentTheme = ref();
+
+function toggleTheme() {
+  settingsStore.setTheme(currentTheme.value);
+  $q.dark.set(currentTheme.value);
+}
+
+onMounted(() => {
+  settingsStore.loadTheme();
+  $q.dark.set(settingsStore.darkTheme);
+  currentTheme.value = settingsStore.darkTheme;
+});
+</script>
